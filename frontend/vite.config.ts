7afe842ceda43f513e7566d61204dd703e086d35
david_c <david.cordeiro@epitech.eu>
2023-03-29 16:09:39 +0200

    [update] vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,24 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    global: 'globalThis',
+    global: "globalThis",
   },
   resolve: {
     alias: {
-      process: 'process/browser',
-      util: 'util',
+      process: "process/browser",
+      util: "util",
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        "@safe-globalThis/safe-apps-provider",
+        "@safe-globalThis/safe-apps-sdk",
+      ],
     },
   },
   plugins: [react()],
-})
+});
