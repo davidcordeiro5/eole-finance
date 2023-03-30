0c56093aba4f8261326c5c0536a2bd32593865bf
david_c <david.cordeiro@epitech.eu>
2023-03-30 12:18:26 +0200

    [add] SimpleStorage SC && update config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+    arbitrumGoerli: {
+      url: `https://arb-goerli.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  solidity: "0.8.16",
 };
 
 export default config;
